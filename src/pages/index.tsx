bfcf84b6e9c83bc3d00d14af75af20cae97a362d
Mohammad Nafisi <nafisidev@gmail.com>
2023-03-09 19:00:28 -0800

    Adding the state management

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import { React, useState }
+import React from 'react'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 const button_style = "border-2 border-sky-500 h-60 w-60"
 
 export default function Home() {
 
-  
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [currentPlayer, setCurrentplayer] = useState(false);
+
+  function Tictac() {
+    setCurrentplayer(!currentPlayer);
+  }
 
   return (
     <>
@@ -19,21 +25,22 @@ export default function Home() {
       </Head>
       <main>
       <h1>Welcome to the Tic Tac Toe Game</h1>
+      <p>It is {currentPlayer}s turn!</p>
       <div className='flex content-center justify-center'>
       <div className='flex flex-col'>
-       <button className={button_style} onClick={Clicked_toe}></button>
-       <button className={button_style} onClick={Clicked_toe}></button>
-       <button className={button_style} onClick={Clicked_toe}></button>
+       <button className={button_style} onClick={Tictac}></button>
+       <button className={button_style} onClick={Tictac}></button>
+       <button className={button_style} onClick={Tictac}></button>
        </div>
        <div className='flex flex-col'>
-       <button className={button_style} onClick={Clicked_toe}></button>
-       <button className={button_style} onClick={Clicked_toe}></button>
-       <button className={button_style} onClick={Clicked_toe}></button>
+       <button className={button_style} onClick={Tictac}></button>
+       <button className={button_style} onClick={Tictac}></button>
+       <button className={button_style} onClick={Tictac}></button>
        </div>
        <div className='flex flex-col'>
-       <button className={button_style} onClick={Clicked_toe}></button>
-       <button className={button_style} onClick={Clicked_toe}></button>
-       <button className={button_style} onClick={Clicked_toe}></button>
+       <button className={button_style} onClick={Tictac}></button>
+       <button className={button_style} onClick={Tictac}></button>
+       <button className={button_style} onClick={Tictac}></button>
        </div>
        </div>
 
