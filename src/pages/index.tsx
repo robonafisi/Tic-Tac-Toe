ce460aff1be0c168f672219d1ebdf1455d00fffd
Mohammad Nafisi <nafisidev@gmail.com>
2023-03-09 22:00:39 -0800

    Adding the id system

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,7 @@ import React from 'react'
 import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
-const button_style = "border-2 border-slate-50 h-60 w-60"
-const obutton_style = "border-2 border-slate-50 h-60 w-60 bg-green-600 text-white text-9xl"
-const xbutton_style = "border-2 border-slate-50 h-60 w-60 bg-red-600 text-white text-9xl"
+const button_style = ["border-2 border-slate-50 h-60 w-60","border-2 border-slate-50 h-60 w-60 bg-green-600 text-white text-9xl", "border-2 border-slate-50 h-60 w-60 bg-red-600 text-white text-9xl"]
 
 
 export default function Home() {
@@ -17,12 +15,18 @@ export default function Home() {
 
 
 
-  const Tictac = event => {
+  const Tictac = () => {
     if(currentPlayer == "X"){
-      console.log(event.target.id)
+      const newBoard = [...currentBoard];
+      console.log(newBoard);
+      const newBoard[1] = 2;
+      // setCurrentboard(newBoard);
       setCurrentplayer("O");
     }
     else{
+      const newBoard = [...currentBoard];
+      // const newBoard[event.target.id] = 1;
+      // setCurrentboard(newBoard);
       console.log(event.target.id)
       setCurrentplayer("X");
     }
@@ -43,19 +47,19 @@ export default function Home() {
       <p className='text-white'>It is {currentPlayer}s turn!</p>
       <div className='flex content-center justify-center'>
       <div className='flex flex-col'>
-       <button className={button_style} id="1" onClick={Tictac}></button>
-       <button className={button_style} id="4" onClick={Tictac}></button>
-       <button className={button_style} id="7" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[0]]} id="1" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[1]]} id="4" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[2]]} id="7" onClick={Tictac}></button>
        </div>
        <div className='flex flex-col'>
-       <button className={button_style} id="2" onClick={Tictac}></button>
-       <button className={button_style} id="5" onClick={Tictac}></button>
-       <button className={button_style} id="8" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[3]]} id="2" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[4]]} id="5" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[5]]} id="8" onClick={Tictac}></button>
        </div>
        <div className='flex flex-col'>
-       <button className={button_style} id="3" onClick={Tictac}></button>
-       <button className={button_style} id="6" onClick={Tictac}></button>
-       <button className={button_style} id="9" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[6]]} id="3" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[7]]} id="6" onClick={Tictac}></button>
+       <button className={button_style[currentBoard[8]]} id="9" onClick={Tictac}></button>
        </div>
        </div>
 
