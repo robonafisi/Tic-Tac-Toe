87d6db3bd912f8262c404e0d632077454182083a
Mohammad Nafisi <nafisidev@gmail.com>
2023-03-10 08:48:56 -0800

    Added the calculate winner function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,10 @@ export default function Home() {
         setCurrentplayer("O");
       }
       else{
-        setPopupwindow(!popupwindow);
+        setPopupwindow(true);
+        setTimeout(() => {
+          setPopupwindow(false);
+        }, 3000);
       }
     }
     else{
@@ -40,7 +43,10 @@ export default function Home() {
       setCurrentplayer("X");
       }
       else{
-        setPopupwindow(!popupwindow);
+        setPopupwindow(true);
+        setTimeout(() => {
+          setPopupwindow(false);
+        }, 3000);
       }
     }
      
@@ -51,7 +57,7 @@ export default function Home() {
   }
 
 
-  function calculateWinner(){
+  function Calculatewinner(){
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -62,6 +68,18 @@ export default function Home() {
       [0, 4, 8],
       [2, 4, 6],
     ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+    
+      if (currentBoard[a] === 1 && currentBoard[a] === 1 && currentBoard[a] === 1) {
+        return console.log("Winner, winner, chicken dinner for X");
+      } else if (currentBoard[a] === 2 && currentBoard[a] === 2 && currentBoard[a] === 2){
+        return console.log("Winner, winner, chicken dinner for O");
+      }
+    }
+    return null;
+  
   }
 
   return (
@@ -86,10 +104,7 @@ export default function Home() {
         <div className='text-2xl text-red-600 flex content-center justify-center'>
           You Cannot Replace a Cell Value
         </div>
-        <div className=''>
-        <button onClick={() => setPopupwindow(false)} className="text-white bg-red-700 hover:bg-red-800 focus:outline-none font-medium rounded-full text-lg px-2 py-1 text-center mr-2 mb-2">
-          Close Pop-up
-        </button>
+        <div className='flex content-center justify-center'>
       </div>  
        </div>
       )}
@@ -97,6 +112,9 @@ export default function Home() {
 
       <div className='flex content-center justify-center m-3'>
         <button className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l border border-white font-medium rounded-lg text-xl px-5 py-2.5 text-center' onClick={Reset}>Reset</button>
+      </div>
+      <div className='flex content-center justify-center m-3'>
+        <button className='text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l border border-white font-medium rounded-lg text-xl px-5 py-2.5 text-center' onClick={Calculatewinner}>Calculate Winner</button>
       </div>
       <div className='flex content-center justify-center'>
       <div className='flex flex-col'>
