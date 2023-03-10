46b079041f933c590a21ee60ec2522ec7f6ed545
Mohammad Nafisi <nafisidev@gmail.com>
2023-03-09 19:39:54 -0800

    Adding the new theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
-const button_style = "border-2 border-sky-500 h-60 w-60"
+const button_style = "border-2 border-slate-50 h-60 w-60"
 
 export default function Home() {
 
@@ -31,13 +31,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main>
       <h1>Welcome to the Tic Tac Toe Game</h1>
       <p>It is {currentPlayer}s turn!</p>
       <div className='flex content-center justify-center'>
       <div className='flex flex-col'>
-       <button className="border-2 border-sky-500 h-60 w-60 bg-green-600 text-white text-9xl" onClick={Tictac}>O</button>
-       <button className="border-2 border-sky-500 h-60 w-60 bg-red-600 text-white text-9xl" onClick={Tictac}>X</button>
+       <button className="border-2 border-slate-50 h-60 w-60 bg-green-600 text-white text-9xl" onClick={Tictac}>O</button>
+       <button className="border-2 border-slate-50 h-60 w-60 bg-red-600 text-white text-9xl" onClick={Tictac}>X</button>
        <button className={button_style} onClick={Tictac}></button>
        </div>
        <div className='flex flex-col'>
